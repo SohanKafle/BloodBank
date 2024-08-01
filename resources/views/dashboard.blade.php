9f83d881eda90441eb60c8d678fee280a63b9bdb
SohanKafle <soka_bca2077@lict.edu.np>
2024-08-02 00:12:51 +0545

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
+@section('content')
+    <h2 class="font-bold text-3xl text-red-500">Dashboard</h2>
+    <hr class="h-1 bg-red-500">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+    <div class="grid grid-cols-3 gap-4 mt-10">
+        <div class="bg-blue-500 p-4 shadow-md rounded-lg">
+            <h3 class="font-bold text-2xl text-white">Total Donors</h3>
+            <p class="text-4xl text-white font-bold">100</p>
+        </div>
+        <div class="bg-red-500 p-4 shadow-md rounded-lg">
+            <h3 class="font-bold text-2xl text-white">Blood Request</h3>
+            <p class="text-4xl text-white font-bold">100</p>
         </div>
     </div>
-</x-app-layout>
+    
+@endsection
